b16d522b5b0a066366e63c45c9e83d7d9b6fe06b
sun558 <78543028+sun558@users.noreply.github.com>
2025-02-17 23:52:04 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ const doFamilyTask = async (cloudClient,index) => {
     } else {
       familyId = familyInfoResp[0].familyId;
     }
-	await delay((Math.random() * 6000) + 3000); // 随机等待3到6秒
+	await delay((Math.random() * 3000) + 3000); // 随机等待3到6秒
 	if(index < accountPerson ){
 		const res = await cloudClient.familyUserSign(familyId);
 		return res.signStatus ? undefined : [res.bonusSpace] ;
